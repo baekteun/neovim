e33772cb9b9df2b36b7a4c291aee44253a13d0fd
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2022-08-12 15:24:37 +0900

    Add lspkind, cmp

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,4 +11,9 @@ packer.startup(function(use)
   use 'norcalli/nvim-colorizer.lua'
   use 'hoob3rt/lualine.nvim'
   use 'neovim/nvim-lspconfig'
+  use 'onsails/lspkind.nvim'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/nvim-cmp'
+  use 'L3MON4D3/LuaSnip'
 end)
