7d94b139c9fe399e09c006c1e507bfc2d6fd560c
변찬우 <chan00106@naver.com>
2022-08-14 18:30:10 +0900

    Add Neotree

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -33,5 +33,14 @@ packer.startup(function(use)
   use 'lewis6991/gitsigns.nvim'
   use 'williamboman/mason.nvim'
   use 'williamboman/mason-lspconfig.nvim'
-  use "EdenEast/nightfox.nvim"
+  use 'EdenEast/nightfox.nvim'
+  use {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+    }
+  }
 end)
