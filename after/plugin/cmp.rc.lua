9de427d69635d9df204187e0125c4c770fb29f3e
변찬우 <chan00106@naver.com>
2022-08-14 16:48:24 +0900

    Add cmp mapping

--- after/plugin/cmp.rc.lua
+++ after/plugin/cmp.rc.lua
@@ -1,7 +1,14 @@
 local status, cmp = pcall(require, "cmp")
+local status2, luasnip = pcall(require, "luasnip")
 if (not status or not cmp) then return end
+if (not status2) then return end
 
 local lspkind = require 'lspkind'
+
+local function has_words_before()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+end
 
 cmp.setup({
   snippet = {
@@ -17,6 +24,22 @@ cmp.setup({
     ['<CR>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true
+    }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expandable() then
+        luasnip.expand()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, {
+      "i",
+      "s",
     }),
   }),
   sources = cmp.config.sources({
